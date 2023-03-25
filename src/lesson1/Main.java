aac3beba7b4c75bc533a59e90fc22ba0ff77f692
Kirill Karavaev <coffee.2016@yandex.ru>
2023-03-25 11:29:12 +0300

    Initial commit

---
+++ src/lesson1/Main.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+import lesson1.task.Task1;
+import lesson1.task.Task2;
+import lesson1.task.Task3;
+
+public class Main {
+    public static void main(String[] args) {
+//        Task1 task1 = new Task1();
+//        Task1.hello();
+//        task1.hello();
+//        Task2.hello();
+        int[] array = new int[] {1,1,1,1,1,0,0,1,1,1,1};
+
+        System.out.println(Task3.getMaxOneCount(array));
+    }
+}
